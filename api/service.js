49d3d282d1757373e6d8b650af5b399a9b53703d
PCBZ <devil02047@outlook.com>
2024-11-23 12:12:22 -0800

    enable https

--- api/service.js
+++ api/service.js
@@ -1,10 +1,21 @@
 import app from "./app.js";
 import dotenv from "dotenv";
+import https from "https";
+import fs from "fs";
 
 dotenv.config();
 
-const PORT = 8000;
+const options = {
+  key: fs.readFileSync("key.pem"),
+  cert: fs.readFileSync("cert.pem"),
+};
 
-app.listen(PORT, () => {
+const PORT = 8443;
+
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
+
+https.createServer(options, app).listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
