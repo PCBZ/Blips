4474727a66b41ddfb539ca909cca4ddd83cc9ca8
PCBZ <devil02047@outlook.com>
2024-11-29 15:30:54 -0800

    change theme color to blue

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,8 +12,10 @@ function App() {
   return (
     <div className="App">
       <nav>
-        <Link to="/">Home</Link>
-        <Link to="/profile">Profile</Link>
+        <div className='nav-container'>
+          <Link to="/">Home</Link>
+          <Link to="/profile">Profile</Link>
+        </div>
       </nav>
       
       <Routes>
