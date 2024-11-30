065502ecb955d04fa7c317f78f20783676e0aa48
PCBZ <devil02047@outlook.com>
2024-11-29 19:34:47 -0800

    change p to label

--- client/src/components/BlipDetail.jsx
+++ client/src/components/BlipDetail.jsx
@@ -212,7 +212,7 @@ function BlipDetail() {
               <ul className="comment-list">
                 {comments.map((comment) => (
                   <li key={comment.id} className="comment-item">
-                    <p className="comment-content">{comment.content}</p>
+                    <label className="comment-content">{comment.content}</label>
                     <small className="comment-meta">
                       <strong>{comment.user.username}</strong> at:{" "}
                       {new Date(comment.updatedAt).toLocaleString()}
